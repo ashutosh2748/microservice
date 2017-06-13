cc54445c2ef0d3f758a458480adcb03ad58e1704
ashutosh <ashutoshghimire@gmail.com>
2017-06-12 22:57:10 -0400

    initial

---
+++ Order-Backend/src/main/java/com/ashutosh/order/rest/Order.java
@@ -0,0 +1,42 @@
+package com.ashutosh.order.rest;
+import java.util.Date;
+
+import com.ashutosh.order.domains.Customer;
+import com.ashutosh.order.domains.Product;
+
+public class Order {
+private long id;
+private long amount;
+private Date dateOrder;
+private Customer customer;
+private Product product;
+public long getId() {
+return id;
+}
+public void setId(long id) {
+this.id = id;
+}
+public long getAmount() {
+return amount;
+}
+public void setAmount(long amount) {
+this.amount = amount;}
+public Date getDateOrder() {
+return dateOrder;
+}
+public void setDateOrder(Date dateOrder) {
+this.dateOrder = dateOrder;
+}
+public Customer getCustomer() {
+return customer;
+}
+public void setCustomer(Customer customer) {
+this.customer = customer;
+}
+public Product getProduct() {
+return product;
+}
+public void setProduct(Product product) {
+this.product = product;
+}
+}
